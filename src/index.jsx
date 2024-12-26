aea6d0ec7d08982011e8e5403dff2eda847f0aa9
daedalus15 <daedalus.4.2.51@gmail.com>
2024-12-26 22:58:48 +0530

    init

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+// src/index.jsx is the entry point for the application.
+// It imports the main scene and styles. It is the main file that is executed when the application is run.
+// It is responsible for importing and initializing the main scene and styles for the application.
+import './scenes/mainScene.js';
+import '../styles.css';
